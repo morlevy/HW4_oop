b2f758f6691bb608d5d5329525c51bb66cd7931a
idan <idankash@gmail.com>
2023-01-11 15:24:30 +0200

    resultimpl

---
+++ Solution/OOPResultImpl.java
@@ -0,0 +1,35 @@
+package OOP.Solution;
+
+import OOP.Provided.OOPResult;
+
+public class OOPResultImpl implements OOPReslt {
+    private String message;
+    private OOPTestResult resultType;
+
+    public OOPResultImpl(OOPTestResult resultType, String message) {
+        this.resultType = resultType;
+        this.message = message;
+    }
+
+    @Override
+    public OOPTestResult getResultType() {
+        return this.resultType;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        OOPResultImpl other = (OOPResultImpl) obj;
+        return this.resultType == other.resultType && this.message.equals(other.message);
+    }
+}
